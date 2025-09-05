23fa0e27c393ce2b09117997f60042c3cf6824d3
AHdev22 <amr.h.dev22@gmail.com>
2025-09-05 12:50:44 +0300

    Alter Table

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from restaurant.models import MenuItem
 # Register your models here.
+admin.site.register(MenuItem)
--- restaurant/models.py
+++ restaurant/models.py
@@ -6,7 +6,7 @@ class MenuItem(models.Model):
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     category = models.CharField(max_length=50)
-    image = models.CharField(max_length=200, blank=True, null=True)  # store static path
+    image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
 
     def __str__(self):
         return self.name
