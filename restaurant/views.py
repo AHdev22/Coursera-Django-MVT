cde3b07e8e9b6f7ee40d14e7a317ce91483d5a6b
AHdev22 <amr.h.dev22@gmail.com>
2025-09-05 12:21:17 +0300

    initial: Logic For MenuItems

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import MenuItem
+from django.views import View
 # Create your views here.
+
+class MenuView(View):
+    def get(self, request):
+        menu_items = MenuItem.objects.all()
+        return render(request, 'main/menu.html', {'menu_items': menu_items})
