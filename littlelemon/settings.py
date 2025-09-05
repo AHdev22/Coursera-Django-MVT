00c65e39724f1e625c31f59f97962b6f90d4c073
AHdev22 <amr.h.dev22@gmail.com>
2025-09-05 11:29:02 +0300

    Apply MySQL in Settings

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -74,8 +74,15 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'your_db_name',      # e.g., 'coursera_mvt'
+        'USER': 'your_db_user',      # e.g., 'root'
+        'PASSWORD': 'your_password', # your MySQL password
+        'HOST': 'localhost',         # '127.0.0.1' if local
+        'PORT': '3306',              # default MySQL port
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'" # To Throw an Error 
+        } 
     }
 }
 
