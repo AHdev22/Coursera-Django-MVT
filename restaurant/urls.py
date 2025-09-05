c8128e6131f2524d3e0c409972fed5e29fe4ad6d
AHdev22 <amr.h.dev22@gmail.com>
2025-09-05 12:30:23 +0300

    Update MenuItems

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path 
+import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('menu-item/', views.MenuView , name='menu-view'),
+    path('item/<int:item_id>', views.MenuDetailView , name='menu-details'),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import MenuItem
 from django.views import View
 # Create your views here.
@@ -6,4 +6,9 @@ from django.views import View
 class MenuView(View):
     def get(self, request):
         menu_items = MenuItem.objects.all()
-        return render(request, 'main/menu.html', {'menu_items': menu_items})
+        return render(request, 'menu.html', {'menu_items': menu_items})
+
+class MenuDetailView(View):
+    def get(self, request, pk):
+        item = get_object_or_404(MenuItem, pk=pk)
+        return render(request, 'item.html', {'item': item})
