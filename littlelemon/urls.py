92c175e9d8965815dfdf13defae6b83c9868da71
AHdev22 <amr.h.dev22@gmail.com>
2025-09-05 11:46:01 +0300

    Apply Media and Static folders

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
+from littlelemon import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
