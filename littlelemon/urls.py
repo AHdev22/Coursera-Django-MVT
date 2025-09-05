09d3448fec88384bebb04ddc8af936cf28f21704
AHdev22 <amr.h.dev22@gmail.com>
2025-09-05 12:37:49 +0300

    initial: Templates

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 
-from littlelemon import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('restaurant/', include('restaurant.urls'))
 ]
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path 
-import views
+from restaurant.views import MenuDetailView ,MenuView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('menu-item/', views.MenuView , name='menu-view'),
-    path('item/<int:item_id>', views.MenuDetailView , name='menu-details'),
+    path('menu-item/', MenuView.as_view() , name='menu-view'),
+    path('item/<int:item_id>', MenuDetailView.as_view() , name='menu-details'),
 ]
